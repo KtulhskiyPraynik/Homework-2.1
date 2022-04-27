f4e20cdfaacb2d0cba430d266abe906000df29c7
Марина Безотеческая <bezotecheskaya@mail.ru>
2022-04-27 12:43:59 +0700

    "Main"

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long amount = 100;
+        int bonus = 20;
+        long miles = amount / bonus;
+        System.out.println("Начислено " + miles + " бонусных миль.");
+    }
+}
